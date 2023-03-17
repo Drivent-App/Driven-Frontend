08a1bf71f76b00d259d3b4743fcd5f17a8045e92
calcado <pecalcado@gmail.com>
2023-03-17 15:34:37 -0300

    feat: service activitiesApi

---
+++ src/hooks/api/useActivities.js
@@ -0,0 +1,21 @@
+import useAsync from '../useAsync';
+import useToken from '../useToken';
+import * as activitiesApi from '../../services/activitiesApi';
+
+export default function useActivities() {
+  const token = useToken();
+
+  const {
+    data: activities,
+    loading: activitiesLoading,
+    error: activitiesError,
+    act: getActivities,
+  } = useAsync(() => activitiesApi.getAllActivities(token));
+
+  return {
+    activities,
+    activitiesLoading,
+    activitiesError,
+    getActivities,
+  };
+}
