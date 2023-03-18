f5d7a85f04e71c59e9efe9531d670b6c61c64113
dcaaz <danicorreaazevedo@gmail.com>
2023-03-18 12:07:42 -0300

    feat: filter all rooms

--- src/components/Activities/Alditory/side.js
+++ src/components/Activities/Alditory/side.js
@@ -1,23 +1,38 @@
 import { Auditory, Element2, Left, Rigth, All } from '../../../style/activetiesStyle';
 import enter from '../../../assets/images/enter.png';
+/* import out from '../../../assets/images/outlined.png'; */
+import { AuthContext } from '../../../contexts/Auth';
+import { useContext } from 'react';
 
 export default function SideAuditorium() {
+  const { filteredActivities } = useContext(AuthContext);
+
+  let local = filteredActivities.filter((el) => el.local === 'Auditório Lateral');
+
   return(
     <>
       <Auditory>
         <h1>Auditório Lateral</h1>
         <All>
-          <Element2>
-            <Left>
-              <h2>Palestra x</h2>
-              <h3>09:00 - 11:00</h3>
-            </Left>
-            <hr/>
-            <Rigth>
-              <img src={enter} alt="Ainda com vagas"/>
-              <h2>27 vagas</h2>
-            </Rigth>
-          </Element2>
+          {
+            local
+              ?
+              local.map((l) =>
+                <Element2 key={l.id}>
+                  <Left>
+                    <h2>{l.lectureName}</h2>
+                    <h3>{l.timeStart}</h3>
+                  </Left>
+                  <hr />
+                  <Rigth>
+                    <img src={enter} /* alterar aqui *//>
+                    <h2>{l.numberVacancies} {l.numberVacancies === 1 ? 'vaga' : 'vagas'}</h2>
+                  </Rigth>
+                </Element2>
+              )
+              :
+              <h1>Sem palestras</h1>
+          }
         </All>
       </Auditory>
     </>
